bb1fdd6080849ae598073905418bca41605887dd
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2025-01-19 19:07:44 +0900

    🐛 refactor FAQ dropdown component for improved readability and styling consistency

--- packages/tapie/src/sections/home/FAQ/styles.css.ts
+++ packages/tapie/src/sections/home/FAQ/styles.css.ts
@@ -1,5 +1,5 @@
-import { Color, Radius, Spacing } from '@tapie-kr/inspire-react/variables'
-import { style } from '@vanilla-extract/css'
+import { Color, Radius, Spacing } from '@tapie-kr/inspire-react/variables';
+import { style } from '@vanilla-extract/css';
 
 export const section = style({
   maxWidth: 1400,
@@ -10,7 +10,7 @@ export const section = style({
       gap: Spacing.Large,
     },
   },
-})
+});
 
 export const maxWidth = style({
   maxWidth: 650,
@@ -19,20 +19,31 @@ export const maxWidth = style({
       maxWidth: '100%',
     },
   },
-})
+});
 
-export const dropdown = style([maxWidth, {
-  width: '100%',
-  padding: 20,
-  background: Color.Surface.Elevated,
-  border: `1px solid ${Color.Line.Border}`,
-  borderRadius: Radius.Default,
-  cursor: 'pointer',
-}])
+export const question = style({
+  userSelect: 'none',
+});
+
+export const answer = style({
+  cursor: 'text',
+});
+
+export const dropdown = style([
+  maxWidth,
+  {
+    width: '100%',
+    padding: 20,
+    background: Color.Surface.Elevated,
+    border: `1px solid ${Color.Line.Border}`,
+    borderRadius: Radius.Default,
+    cursor: 'pointer',
+  },
+]);
 
 export const content = style({
   overflow: 'hidden',
-})
+});
 export const rotatedIcon = style({
   transform: 'rotate(180deg)',
-})
+});
