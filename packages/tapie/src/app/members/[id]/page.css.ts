5485b403d9cc73ddbe23f1d2b03fd1de0ac402e3
Taeyeong Kim <taeyeong.kim@algorix.io>
2025-02-02 14:09:30 +0900

    🎨 add MembersDetailPage component and update code guidelines

---
+++ packages/tapie/src/app/members/[id]/page.css.ts
@@ -0,0 +1,4 @@
+import { getShorthandedValue, spacingVars } from '@tapie-kr/inspire-react';
+import { style } from '@vanilla-extract/css';
+
+export const container = style({ padding: getShorthandedValue(spacingVars.medium, spacingVars.base) });
--- packages/tapie/src/app/members/[id]/page.tsx
+++ packages/tapie/src/app/members/[id]/page.tsx
@@ -1,3 +1,27 @@
+import { container } from './page.css';
+
+import { AspectRatio, radiusVars, Skeleton, spacingVars, Stack, StackJustify, VStack } from '@tapie-kr/inspire-react';
+
 export default function MembersDetailPage() {
-  return null;
+  return (
+    <Stack
+      fullWidth
+      className={container}
+      spacing={spacingVars.medium}
+      justify={StackJustify.CENTER}
+    >
+      <AspectRatio
+        ratio={1}
+        width={130}
+      >
+        <Skeleton
+          fullWidth
+          fullHeight
+          borderRadius={radiusVars.full}
+        />
+      </AspectRatio>
+
+      <VStack />
+    </Stack>
+  );
 }
