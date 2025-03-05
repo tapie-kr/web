bcc7ef8e35c604411293eeee3909d59695f80b33
FIRO <72495729+iamfiro@users.noreply.github.com>
2025-03-06 03:08:45 +0900

    🔧 update profile image styles to include global border radius for images

--- packages/tapie/src/sections/portfolios/index/Members/styles.css.ts
+++ packages/tapie/src/sections/portfolios/index/Members/styles.css.ts
@@ -1,6 +1,6 @@
 import { colorVars, getResponsiveQuery, radiusVars } from '@tapie-kr/inspire-react';
 
-import { style } from '@vanilla-extract/css';
+import { globalStyle, style } from '@vanilla-extract/css';
 
 export const profileImage = style({
   width:        80,
@@ -12,3 +12,5 @@ export const profileImage = style({
     height: 70,
   }),
 });
+
+globalStyle(`${profileImage} img`, { borderRadius: radiusVars.full });
