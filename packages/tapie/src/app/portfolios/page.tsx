29759602d7bde29320a0dbd3c8b99336366fb8e2
FIRO <72495729+iamfiro@users.noreply.github.com>
2025-03-06 13:54:25 +0900

    섹션 you OUT

--- packages/tapie/src/app/portfolios/page.tsx
+++ packages/tapie/src/app/portfolios/page.tsx
@@ -1,7 +1,7 @@
 import Hero from '@/sections/portfolios/index/Hero';
 import Members from '@/sections/portfolios/index/Members';
 import Projects from '@/sections/portfolios/index/Projects';
-import RecentAwards from '@/sections/portfolios/index/RecentAwards';
+// import RecentAwards from '@/sections/portfolios/index/RecentAwards';
 import RecentProjects from '@/sections/portfolios/index/RecentProjects';
 
 export default function PortfolioPage() {
@@ -9,7 +9,7 @@ export default function PortfolioPage() {
     <>
       <Hero />
       <RecentProjects />
-      <RecentAwards />
+      {/* <RecentAwards /> */}
       <Members />
       <Projects />
     </>
