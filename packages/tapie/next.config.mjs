1c6e93215970e0be2ee9632650b1f2d67a8f8daa
FIRO <72495729+iamfiro@users.noreply.github.com>
2025-03-09 19:16:17 +0900

    🔧 refactor Sentry configuration for improved readability; remove example API route and update ApplyPage logic to handle unavailable form data

--- packages/tapie/next.config.mjs
+++ packages/tapie/next.config.mjs
@@ -1,39 +1,46 @@
-import {withSentryConfig} from '@sentry/nextjs';
+import { withSentryConfig } from '@sentry/nextjs';
 import { withTAPIEWebConfig } from '@tapie-kr/web-shared/configs/nextjs.mjs';
 
 export default withSentryConfig(withTAPIEWebConfig(), {
-// For all available options, see:
-// https://www.npmjs.com/package/@sentry/webpack-plugin#options
 
-org: "tapie-ne",
-project: "tapie-web",
+  /*
+   * For all available options, see:
+   * https://www.npmjs.com/package/@sentry/webpack-plugin#options
+   */
 
-// Only print logs for uploading source maps in CI
-silent: !process.env.CI,
+  org:     'tapie-ne',
+  project: 'tapie-web',
 
-// For all available options, see:
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+  // Only print logs for uploading source maps in CI
+  silent: !process.env.CI,
 
-// Upload a larger set of source maps for prettier stack traces (increases build time)
-widenClientFileUpload: true,
+  /*
+   * For all available options, see:
+   * https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+   */
 
-// Automatically annotate React components to show their full name in breadcrumbs and session replay
-reactComponentAnnotation: {
-enabled: true,
-},
+  // Upload a larger set of source maps for prettier stack traces (increases build time)
+  widenClientFileUpload: true,
 
-// Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-// This can increase your server load as well as your hosting bill.
-// Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-// side errors will fail.
-tunnelRoute: "/monitoring",
+  // Automatically annotate React components to show their full name in breadcrumbs and session replay
+  reactComponentAnnotation: { enabled: true },
 
-// Automatically tree-shake Sentry logger statements to reduce bundle size
-disableLogger: true,
+  /*
+   * Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
+   * This can increase your server load as well as your hosting bill.
+   * Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
+   * side errors will fail.
+   */
+  tunnelRoute: '/monitoring',
 
-// Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-// See the following for more information:
-// https://docs.sentry.io/product/crons/
-// https://vercel.com/docs/cron-jobs
-automaticVercelMonitors: true,
+  // Automatically tree-shake Sentry logger statements to reduce bundle size
+  disableLogger: true,
+
+  /*
+   * Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
+   * See the following for more information:
+   * https://docs.sentry.io/product/crons/
+   * https://vercel.com/docs/cron-jobs
+   */
+  automaticVercelMonitors: true,
 });
