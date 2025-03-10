b81247f7b2e935fc084e381528a793fc53139fc1
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2025-03-10 10:28:37 +0900

    🔧 update layout and spacing in ApplyTitle component for improved readability

--- packages/tapie/src/sections/apply/Title/index.tsx
+++ packages/tapie/src/sections/apply/Title/index.tsx
@@ -18,16 +18,23 @@ export default function ApplyTitle(_props: Props) {
   return (
     <VStack spacing={spacingVars.base}>
       <TAPIESymbol size={TAPIESymbolSize._32} />
-      <VStack spacing={spacingVars.mini}>
-        <Typo.Moderate
+      <VStack spacing={spacingVars.micro}>
+        <VStack spacing={spacingVars.mini}>
+          <Typo.Moderate
+            weight={Weight.MEDIUM}
+            color={colorVars.content.default}
+          >
+            세상의 간극을 같이 이어붙일
+          </Typo.Moderate>
+          <Typo.Medium weight={Weight.SEMIBOLD}>
+            {title}
+          </Typo.Medium>
+        </VStack>
+        <Typo.Tiny
           weight={Weight.MEDIUM}
           color={colorVars.content.default}
-        >
-          세상의 간극을 같이 이어붙일
-        </Typo.Moderate>
-        <Typo.Medium weight={Weight.SEMIBOLD}>
-          {title}
-        </Typo.Medium>
+        >모든 내용은 자동으로 저장됩니다.
+        </Typo.Tiny>
       </VStack>
     </VStack>
   );
